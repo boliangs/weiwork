6ef2efdb6d55d83cc647a425b2624fac80d50b76
泊凉入侵波兰 <2295141047@stu.hqu.edu.cn>
2025-03-05 01:13:26 +0800

    一卡通

---
+++ utils/httpUtil/constant.go
@@ -0,0 +1,32 @@
+package httpUtil
+
+// 头部类型
+const (
+	NormalHeaderType   = "application/json"
+	ScheduleHeaderType = "application/x-www-form-urlencoded;charset=UTF-8"
+)
+
+//URL
+
+const (
+	GsSession_URL    = "h"
+	Emaphome_WEU_URL = "h"
+	Schedule_URL     = "h"
+	Gpa_URL          = "h"
+	Grade_URL        = "h"
+	Xuefen_URL       = "h"
+	ECard_URL        = "h"
+	Card_URL         = "h"
+	Pay_URL          = "h"
+	Book_URL         = "h"
+	Gpa_All_URL      = "h"
+	Gpa_Year_URL     = "h"
+	Gpa_Semester_URL = "h"
+	//Emaphome_WEU_URL = "https://jwapp.hqu.edu.cn/jwapp/sys/emaphome/portal/index.do?forceCas=1"
+	//Schedule_WEU_URL = "https://jwapp.hqu.edu.cn/jwapp/sys/cjcx/*default/index.do?EMAP_LANG=zh"
+	//Schedule_URL     = "https://jwapp.hqu.edu.cn/jwapp/sys/wdkb/modules/xskcb/cxxszhxqkb.do"
+	//TWFID_URL        = "https://w.hqu.edu.cn/por/login_auth.csp"
+	//Emaphome_WEU_URL = "http://jwapp-hqu-edu-cn-s.w.hqu.edu.cn:8118/jwapp/sys/emaphome/portal/index.do?forceCas=1"
+	//Schedule_WEU_URL = "http://jwapp-hqu-edu-cn-s.w.hqu.edu.cn:8118/jwapp/sys/cjcx/*default/index.do?EMAP_LANG=zh"
+	//Schedule_URL     = "http://jwapp-hqu-edu-cn-s.w.hqu.edu.cn:8118/jwapp/sys/wdkb/modules/xskcb/cxxszhxqkb.do?sf_request_type=ajax"
+)
